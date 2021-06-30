57c54f63adc2c8e82e3f9e658abccb28eeac3f61
Dennis Bohn <dennis.bohn@bohnmedia.de>
2021-06-30 15:22:53 +0200

    Bugfix

--- src/Controller/BackupController.php
+++ src/Controller/BackupController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 class BackupController
 {
 
-  public function loadAction($alias): Response
+  public function loadAction(): Response
   {
 		return new Response("Hello World!");
   }
