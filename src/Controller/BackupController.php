c0d457e85d6ec535d55e69e7c0d39bf884343204
Dennis Bohn <dennis.bohn@bohnmedia.de>
2021-06-30 15:13:11 +0200

    Bugfixing

--- src/Controller/BackupController.php
+++ src/Controller/BackupController.php
@@ -2,37 +2,15 @@
 
 namespace BohnMedia\ContaoBackupBundle\Controller;
 
-use Contao\CoreBundle\Framework\ContaoFramework;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
 
 class BackupController
 {
-  protected $rootDir;
-  protected $session;
-  protected $framework;
-
-  public function __construct(string $rootDir, Session $session, ContaoFramework $framework)
-  {
-      $this->rootDir      = $rootDir;
-      $this->session      = $session;
-      $this->framework    = $framework;
-  }
 
   public function loadAction($alias): Response
   {
-    // Initialize framework
-    $this->framework->initialize();
-
-		// Response
-		$response = new Response("Controller works!");
-
-    // Save session
-    $this->session->save();
-
-    // Return
-		return $response;
-
+		return new Response("Hello World!");
   }
+
 }
 		
