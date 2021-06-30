2ce7f8726335741d4ad09aeeda117c23e4aeb404
Dennis Bohn <dennis.bohn@bohnmedia.de>
2021-06-30 20:53:33 +0200

    Einstellungen erweitert

---
+++ src/Resources/contao/dca/tl_settings.php
@@ -0,0 +1,8 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = preg_replace('(?=\{cron_legend\b)', '{backup_legend},backupKey;', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['backupKey'] = [
+    'inputType'               => 'text',
+    'eval'                    => ['tl_class'=>'w50']
+];
