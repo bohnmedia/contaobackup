90b7d13361e01eb645572e1a8e1796fb8da1e094
Dennis Bohn <dennis.bohn@bohnmedia.de>
2021-06-30 13:49:24 +0200

    Basiskonfiguration

---
+++ src/ContaoBackupBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BohnMedia\ContaoBackupBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ContaoBackupBundle extends Bundle
+{
+}
---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace BohnMedia\ContaoBackupBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use BohnMedia\ContaoBackupBundle\ContaoBackupBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(ContaoBackupBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
