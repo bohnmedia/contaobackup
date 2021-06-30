81a1f8d54cc274784e1c266fa1758a485cf6fe18
Dennis Bohn <dennis.bohn@bohnmedia.de>
2021-06-30 22:07:55 +0200

    Config-Test

--- src/Controller/BackupController.php
+++ src/Controller/BackupController.php
@@ -5,23 +5,27 @@ namespace BohnMedia\ContaoBackupBundle\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\Config;
 
 class BackupController
 {
 
   private $framework;
+  private $config;
 
   public function __construct(ContaoFramework $framework)
   {
       $this->framework = $framework;
+      $this->config = $this->framework->getAdapter(Config::class);
   }
   
   public function loadAction(Request $request): Response
   {
-    
-    $key = $request->query->get('key');
 
-		return new Response("Hello World! " . $key);
+    $serverKey = $this->config->get('backupKey');
+    $clientKey = $request->query->get('key');
+
+		return new Response("Hello World! / " . $clientKey . " / " . $serverKey);
 
   }
 
