d5b557eee8b521afc22a0decb5d3ca180e4003a7
Dennis Bohn <dennis.bohn@bohnmedia.de>
2021-06-30 21:51:14 +0200

    Framework im Controller hinzugefügt

--- src/Controller/BackupController.php
+++ src/Controller/BackupController.php
@@ -4,16 +4,22 @@ namespace BohnMedia\ContaoBackupBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Contao\CoreBundle\Framework\ContaoFramework;
 
 class BackupController
 {
 
+  private $framework;
+
+  public function __construct(ContaoFramework $framework)
+  {
+      $this->framework = $framework;
+  }
+  
   public function loadAction(Request $request): Response
   {
     
     $key = $request->query->get('key');
-
-    var_dump($key);
 
 		return new Response("Hello World! " . $key);
 
