88137d7dc74e14468a3842d1b2f470fa8f72efbf
Dennis Bohn <dennis.bohn@bohnmedia.de>
2021-07-01 15:54:53 +0200

    DefaultBackupKey Generierung

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,3 +1,15 @@
 <?php
 
-$GLOBALS['TL_CONFIG']['backupKey'] = 'Das ist ein Test.';
+function generateDefaultBackupKey() {
+
+    $output = '';
+    $allowedChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $max = strlen($allowedChars) - 1;
+    for ($i=0; $i<32; $i++) {
+        $output .= substr($allowedChars, rand(0, $max), 1);
+    }
+    return $output;
+
+}
+
+$GLOBALS['TL_CONFIG']['backupKey'] = generateDefaultBackupKey();
