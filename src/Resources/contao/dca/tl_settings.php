c6765ffa946e700d874bcdfe30421a09666071b5
Dennis Bohn <dennis.bohn@bohnmedia.de>
2021-07-06 15:57:40 +0200

    Referenziele in DCA auf Label

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -3,6 +3,7 @@
 $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = preg_replace('/(?=\{files_legend\b)/', '{backup_legend},backupKey;', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['backupKey'] = [
+    'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['backupKey'],
     'inputType'               => 'text',
     'eval'                    => ['tl_class'=>'w50']
 ];
