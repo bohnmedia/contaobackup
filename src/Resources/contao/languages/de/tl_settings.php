403312c1bbe6418de1ba073b35771e721a77d2cf
Dennis Bohn <dennis.bohn@bohnmedia.de>
2021-07-06 15:53:27 +0200

    Übersetzung von xlf auf php umgestellt

---
+++ src/Resources/contao/languages/de/tl_settings.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_settings']['backup_legend'] = 'Backup-Einstellungen';
+$GLOBALS['TL_LANG']['tl_settings']['backupKey'] = ['Backup-Key', 'Dieser Schlüssel wird in der URL zum Download eines Backups verwendet.'];
---
+++ src/Resources/contao/languages/en/tl_settings.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_settings']['backup_legend'] = 'Backup-Settings';
+$GLOBALS['TL_LANG']['tl_settings']['backupKey'] = ['Backup-Key', 'Key used in the url to download a backup.'];
