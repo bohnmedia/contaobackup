dfd09f20f6f254d10458ddb96153b50da04c5cd4
Dennis Bohn <dennis.bohn@bohnmedia.de>
2021-07-06 15:43:32 +0200

    Optionen fehlten in Contao 4.4

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = preg_replace('/(?=\{cron_legend\b)/', '{backup_legend},backupKey;', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = preg_replace('/(?=\{files_legend\b)/', '{backup_legend},backupKey;', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['backupKey'] = [
     'inputType'               => 'text',
